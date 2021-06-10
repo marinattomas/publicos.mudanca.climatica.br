60d148ca126aabb68c47357056088f8ce2736cd5
Marina Tomás <marinattomas@gmail.com>
2021-06-10 14:16:03 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,35 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+from app import app
+from apps import home, grupos, campanhas, publico, como, sobre
+
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+
+
+@app.callback(Output('page-content', 'children'),
+              Input('url', 'pathname'))
+def display_page(pathname):
+    if pathname == '/apps/home':
+        return home.layout
+    elif pathname == '/apps/grupos':
+        return grupos.layout
+    elif pathname == '/apps/campanhas':
+        return campanhas.layout
+    elif pathname == '/apps/publico':
+        return publico.layout
+    elif pathname == '/apps/como':
+        return como.layout
+    elif pathname == '/apps/sobre':
+        return sobre.layout
+    else:
+        return home.layout
+
+if __name__ == '__main__':
+    app.run_server(debug=True)

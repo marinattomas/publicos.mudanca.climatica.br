099878208110066c7a59181a086494ebad539e47
Marina Tomás <marinattomas@gmail.com>
2023-03-18 17:23:21 -0300

    Delete app.py

--- app.py
+++
@@ -1,5 +0,0 @@
-import dash
-import dash_bootstrap_components as dbc
-
-app = dash.Dash(__name__, suppress_callback_exceptions=True,external_stylesheets=[dbc.themes.MINTY])
-app.title = 'Grupos de percepção das Mudanças climáticas BR'

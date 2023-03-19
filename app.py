bc94097f6d0426fff8991e9f1e1faf5a9b654743
Marina Tomás <marinattomas@gmail.com>
2023-03-19 10:19:41 -0300

    deploy correction

--- app.py
+++ app.py
@@ -4,4 +4,3 @@ import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True,external_stylesheets=[dbc.themes.MINTY])
 app.title = 'Grupos de percepção das Mudanças climáticas BR'
-server = app.server
